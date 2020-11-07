55f6016be4fff529ed5c0d62524eb2783bf64625
TeemuSoininen <teemu.soininen@tuni.fi>
2020-11-07 20:22:33 +0200

    Päivitelty hakualueet ja luokittelu

--- haku_ja_alueet.py
+++ haku_ja_alueet.py
@@ -12,22 +12,22 @@ mappays = pd.read_csv('haku_alue_mappays.csv')
 alkuparsittu = pd.read_csv('alkuparsinta.csv')
 
 #%% 
-mappays = mappays[['id','osa_alueet']]
+mappays = mappays.drop("Unnamed: 0",axis=1)
 mappays = mappays.set_index('id')
 
 data = alkuparsittu[['unique', 'haku_id']]
-
-data['osa_alue'] = 1
+#%%
+data=data.merge(mappays,how="left",left_on="haku_id",right_on=mappays.index)
 
 #%%
 
-i = 0
+# i = 0
 
-for i in range(0, len(data)):
-    haku_id = data.loc[i, ['haku_id']][0]
-    osa_alue = mappays.at[haku_id, 'osa_alueet']
+# for i in range(0, len(data)):
+#     haku_id = data.loc[i, ['haku_id']][0]
+#     osa_alue = mappays.at[haku_id, 'osa_alueet']
     
-    data.osa_alue.iloc[[i]] = osa_alue
+#     data.osa_alue.iloc[[i]] = osa_alue
 
 #%% 
 
--- luokittelut.py
+++ luokittelut.py
@@ -21,76 +21,15 @@ osa_alueet  = { 0: '01 Varhaiskasvatus', 1: '02 Esi- ja perusopetus', 2: '03 Toi
                5: '06 Järjestötoiminta', 6: '07 Vapaa sivistystyö', 7: '08 Muut'}
 
 
-osa_alueet = [{3: '04 Ammatillinen koulutus'},
-                      {0: '01 Varhaiskasvatus'},
-                      {5: '06 Järjestötoiminta'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {0: '01 Varhaiskasvatus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {6: '07 Vapaa sivistystyö'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {0: '01 Varhaiskasvatus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {5: '06 Järjestötoiminta'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {0: '01 Varhaiskasvatus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {4: '05 Korkeakoulu'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {0: '01 Varhaiskasvatus'},
-                      {4: '05 Korkeakoulu'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {5: '06 Järjestötoiminta'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {5: '06 Järjestötoiminta'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {4: '05 Korkeakoulu'},
-                      {1: '02 Esi- ja perusopetus'},
-                      {3: '04 Ammatillinen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {2: '03 Toisen asteen koulutus'},
-                      {2: '03 Toisen asteen koulutus'}                      
-                      ]
-haku['osa_alueet'] = osa_alueet
-
-id_alue_df = haku[['id', 'osa_alueet']]
-
+osa_alueet_id = ["3","0","5","1","1","3","3","3","3","0","1","1","1","2","3","6","3","3","3","1","1","1","0","1","5","3","1","3","3","0","2","2","2","1","4","3","3","3","3","1","1","1","2","1","3","2","0","4","1","1","5","1","2","2","5","2","4","1","3","2","2","2"]
 #%%
-
-id_alue_df.to_csv("haku_alue_mappays.csv")
-
-
-morso = id_alue_df['osa_alueet'][0]
+uusi_df=pd.DataFrame(index=haku.index,data=osa_alueet_id)
+uusi_df["id"]=haku["id"]
+uusi_df["osa_alue_id"]=uusi_df.loc[:,0].astype(int)
+osa_alue_df=pd.Series(osa_alueet)
+osa_alue_df=osa_alue_df.rename('osa_alue_nimi')
+uusi_df=uusi_df.merge(osa_alue_df,how="left",left_on="osa_alue_id",right_on=osa_alue_df.index)
+uusi_df=uusi_df.drop(0,axis=1)
+uusi_df["osa_alue_id"]=uusi_df["osa_alue_id"]+1
+#%%
+uusi_df.to_csv("haku_alue_mappays.csv")
