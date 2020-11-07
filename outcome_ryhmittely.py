c186045d51829ab105afae97c084f42b25c1984b
Meri Turunen <morso>
2020-11-07 13:40:35 +0200

    outcomeden_ryhmittely

---
+++ outcome_ryhmittely.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov  7 13:36:24 2020
+
+@author: Meri
+"""
+import pandas as pd
+import json
+
+#%%
+tuotokset = pd.read_csv('project_outcome.csv')
+
+
+
